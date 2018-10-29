ee32ac17b0eb763f6bbdb9374f9a3124039333ac
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-10-29 22:29:58 +0100

    add adapter for native rb

---
+++ lib/complexity/structures/native_red_black.rb
@@ -0,0 +1,28 @@
+class NativeRedBlack
+
+    def initialize(array: [])
+        @tree = Containers::CRBTreeMap.new
+        array.sort!
+        array.each do |x|
+            @tree.push(x, x)
+        end
+    end
+
+    def pop
+        [@tree.delete_min, self]
+    end
+
+    def push(n)
+        @tree.push(n, n)
+        self
+    end
+
+    def min
+        @tree.min_key
+    end
+
+    def max
+        @tree.max_key
+    end
+
+end
--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@ require_relative 'lib/complexity/structures/fibonacci_heab'
 require_relative 'lib/complexity/structures/avl_tree'
 require_relative 'lib/complexity/structures/immutable_avl_tree'
 require_relative 'lib/complexity/structures/ruby_red_black'
+require_relative 'lib/complexity/structures/native_red_black'
 require_relative 'test/complexity/tester'
 require_relative 'test/complexity/csv_exporter'
 class RubyDefault
@@ -21,7 +22,7 @@ def random(arr, lo, hi)
     return arr[lo..hi].sample
 end
 
-tester = Tester.new(arr_len: 8, structs: [Heap, FibonacciHeap, ImmutableHeap, AVLTree, ImmutableAVLTree, RubyRedBlack])
+tester = Tester.new(arr_len: 8, structs: [Heap, FibonacciHeap, ImmutableHeap, AVLTree, ImmutableAVLTree, RubyRedBlack, NativeRedBlack])
 csv_exporter = CSVExporter.new
 
 insertion, sizes = tester.execute_all
